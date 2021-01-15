d3cd82602bef2b63f43133eed3a96144fed6459c
CHEMS ED-DINE RACHIDI <chemsed.dinerachidi@gmail.com>
2021-01-15 20:58:24 +0100

    Updated About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,9 @@ const About = () => {
           <p>
             Software Developer and an Indie Game Developer, currently in love
             with React and GraphQL technologies, also working on a 3D platformer
-            game called FEZBOT made using Unity and Blender.
+            game called FEZBOT
           </p>
+          <p>made using Unity and Blender.</p>
           <p> To follow more on my game and more, please visit my itch.io page : </p>
           <p> {"> "} <a target="_blank" href="https://starshums.itch.io">starshums.itch.io</a> </p>
           <p> also available on Game Jolt: </p>
